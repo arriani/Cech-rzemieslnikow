a3dc4258d1391e0daf2ed3556eb66550a7fe74e2
arriani <136384384+arriani@users.noreply.github.com>
2024-09-14 21:24:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// TOGGLE MENU
+
+var navLinks = document.getElementById("navLinks");
+
+    function showMenu (){
+      navLinks.style.right = "0";
+    }
+    function hideMenu (){
+      navLinks.style.right = "-200";
+    }
