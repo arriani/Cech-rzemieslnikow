8bfa37d4946db48beed85cdfabe13e09b445d22a
arriani <136384384+arriani@users.noreply.github.com>
2024-10-03 03:43:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ var navLinks = document.getElementById("navLinks");
     function hideMenu (){
       navLinks.style.right = "-200";
     }
+
+// GALERIA
+
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
+
+function openFullImg(pic){
+  fullImgBox.style.display = "flex";
+  fullImg.src = pic;
+}
+
+function closeFullImg(){
+  fullImgBox.style.display = "none";
+}
+
